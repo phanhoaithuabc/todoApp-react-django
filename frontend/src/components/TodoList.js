a5cc231b153c8ad5282c841d07c48fd5d5d7828a
Phan Hoai Thu <phanhoaithuabc@gmail.com>
2022-06-10 16:58:58 +0700

    todoList update

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -7,6 +7,9 @@ import Modal from "react-bootstrap/Modal";
 import { MdCheckBox, MdCheckBoxOutlineBlank, MdEdit, MdDelete } from "react-icons/md";
 
 export default function TodoList({ todos = [], setTodos }){
+    const [show, setShow] = useState(false);
+    const [record, setRecord] = useState(null);
+
     const renderListGroupItem = (t) => {
         return <ListGroup.Item key={t.id} className="d-flex justify-content-between align-items-center">
             <div className="d-flex justify-content-center">
@@ -17,12 +20,24 @@ export default function TodoList({ todos = [], setTodos }){
                 <span> {t.name} </span>
             </div>
             <div>
-                <MdEdit style={{cursor: "pointer", marginRight: "12px"}} />
+                <MdEdit style={{cursor: "pointer", marginRight: "12px"}} 
+                    onClick={() => { setRecord(t); setShow(true); }}/>
                 <MdDelete style={{cursor: "pointer"}} />
             </div>
         </ListGroup.Item>
     }
 
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    const handleChange = (e) => {
+        setRecord({
+            ...record,
+            name: e.target.value
+        })
+    }
+    
     const handleUpdate = async (id, value) => {
         return axios.patch(`/api/todos/${id}/`, value).then((res) => {
             const {data} = res;
@@ -38,12 +53,12 @@ export default function TodoList({ todos = [], setTodos }){
         <ListGroup>
             {todos.map(renderListGroupItem)}
         </ListGroup>
-        <Modal show={true}>
+        <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Edit Todo</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <FormControl value={""} />
+                <FormControl value={record ? record.name : ""} onChange={handleChange} />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={null}>
